b1c4d05b3585fa355d7912b47bf9e86d94e5e69c
calzadaharo <calzadaharo@gmail.com>
2022-01-13 10:31:33 +0100

    Classpath update

--- one-partition-conf.py
+++ one-partition-conf.py
@@ -1,13 +1,13 @@
-#! /home/rodrigo/yes/bin/python
+#! /usr/bin/python
 
 import os, sys
 
 os.environ['RAPHTORY_BUILD_SERVERS']="1"
 os.environ['RAPHTORY_PARTITION_SERVERS']="1"
 os.environ['RAPHTORY_BUILDERS_PER_SERVER']="1"
-os.environ['RAPHTORY_PARTITIONS_PER_SERVER']="1"
+os.environ['RAPHTORY_PARTITIONS_PER_SERVER']="8"
 
 # Change the RAM asigned to Java (Scala)
-os.environ["JAVA_OPTS"]="-Xmx2G -XX:MaxPermSize=2G -Xss2M"
+os.environ["JAVA_OPTS"]="-Xmx128G -XX:MaxPermSize=128G -Xss128M"
 
-os.system('scala ' + sys.argv[1])
+os.system('scala -classpath ' + sys.argv[1] +" es.dit.upm.Runner")
